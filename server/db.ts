e0185871571e18da29f7251adc8181235acfa1e8
arhammunir1104 <munirarham70@gmail.com>
2025-04-02 02:28:07 +0500

    Fixing db issues

--- server/db.ts
+++ server/db.ts
@@ -7,8 +7,6 @@ import * as dotenv from "dotenv";
 dotenv.config();
 neonConfig.webSocketConstructor = ws;
 
-
-
 if (!process.env.DATABASE_URL) {
   throw new Error(
     "DATABASE_URL must be set. Did you forget to provision a database?",
